e88cf3a0a81d2a4888e707698ef7eb80b1414672
jay johnson <jayj10080@gmail.com>
2018-01-31 02:53:30 +0000

    google map currently working

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'places#index'
-  get 'home', to: 'places#home'
+  root 'places#home'
   get 'resources', to: 'places#resources'
+  resources :places, only: [:index]
   resources :helpers, only: [:index, :new, :create, :show]
   resources :helpees, only: [:index, :new, :create, :show]
 end

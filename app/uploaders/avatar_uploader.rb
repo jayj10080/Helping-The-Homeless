d02580d76c5902769d9e49cb6a89c4d7d19c8139
jay johnson <jayj10080@gmail.com>
2018-02-11 18:57:18 +0000

    added quote position relative over image, added mission statement, modified css

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -23,7 +23,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process resize_to_fill: [500, 400]
+  process resize_to_fill: [250, 250]
   # process scale: [200, 300]
   #
   # def scale(width, height)

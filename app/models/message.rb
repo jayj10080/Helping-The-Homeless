39bd4e2aa8de404b26671d24b3efa420e86a24b1
jay johnson <jayj10080@gmail.com>
2018-02-12 23:52:19 +0000

    added timestamp to chat room messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,6 @@ class Message < ApplicationRecord
   after_create :send_to_firebase
 
   def send_to_firebase
-    FIREBASE.push("message", { :id => self.id, :body => self.body, :'.priority' => 1 })
+    FIREBASE.push("message", { :id => self.id, :body => self.body, :'.priority' => 1, :timestamp => self.created_at })
   end
 end

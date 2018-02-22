8ff16941f14b4831aeca70138de907b6618474f1
jay johnson <jayj10080@gmail.com>
2018-02-22 23:33:24 +0000

    Testing action mailer on production

--- app/models/direct_message.rb
+++ app/models/direct_message.rb
@@ -1,8 +1,15 @@
 class DirectMessage < ActiveRecord::Base
- belongs_to :conversation
- belongs_to :user
- validates_presence_of :body, :conversation_id, :user_id
- def message_time
-  created_at.strftime("%m/%d/%y at %l:%M %p")
- end
+  belongs_to :conversation
+  belongs_to :user
+  validates_presence_of :body, :conversation_id, :user_id
+  after_create :send_message_email
+ 
+  def message_time
+    created_at.strftime("%m/%d/%y at %l:%M %p")
+  end
+
+  def send_message_email
+    NotificationMailer.new_message(self).deliver_now
+  end
+
 end

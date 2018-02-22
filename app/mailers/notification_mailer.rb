c28973c840f22f86e8d09abd523e8c6e2789ed36
jay johnson <jayj10080@gmail.com>
2018-02-22 18:25:45 +0000

    Method 2 for css on landing page, notification mailer seems to be working

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,11 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@Helping-The-Homeless.com"
 
-  def new_message
-    mail(to: "jjcoder1000@gmail.com",
+  def new_message(message)
+    @conversation = message.conversation
+    @person = @conversation.recipient_id
+    @user = User.find(@person)
+    mail(to: @user.email,
       subject: "You have received a new message.")
   end
 end

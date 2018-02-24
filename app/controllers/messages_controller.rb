8bb5ccb77174666e89d102e02ffbffb3a88fb1a8
jay johnson <jayj10080@gmail.com>
2018-02-24 16:42:14 +0000

    Locked down chatroom against users without profiles

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,21 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!
   def index
-    @message = Message.new
-    @messages = Message.order(:id).all
+    if current_user.helpee.present? || current_user.helper.present?
+      @message = Message.new
+      @messages = Message.order(:id).all
+    else 
+      return render plain: 'You need to create a profile first.', status: :forbidden
+    end
   end
 
   def create
-    Message.create(message_params.merge({user_id: current_user.id, email: current_user.email}))
-    redirect_to messages_path
+    if current_user.helpee.present? || current_user.helper.present?
+      Message.create(message_params.merge({user_id: current_user.id, email: current_user.email}))
+      redirect_to messages_path
+    else
+      return render plain: 'You need to create a profile first.', status: :forbidden
+    end
   end
 
   private

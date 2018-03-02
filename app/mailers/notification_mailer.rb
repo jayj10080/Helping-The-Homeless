19f726d4c89be9f8fcdc7238b2c98d730cc8272c
jay johnson <jayj10080@gmail.com>
2018-03-02 01:03:47 +0000

    if statements in effect

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -5,15 +5,15 @@ class NotificationMailer < ApplicationMailer
     @conversation = message.conversation
     @person = @conversation.recipient_id
     @user = User.find(@person)
-    # if @user.helpee.present?
-    #   if @user.helpee.notifications == true  
+    if @user.helpee.present?
+      if @user.helpee.notifications == true  
         mail(to: @user.email, subject: "You have received a new message.")
-    #   end
-    # end
-    # if @user.helper.present?
-    #   if @user.helper.notifications == true  
-    #     mail(to: @user.email, subject: "You have received a new message.")
-    #   end
-    # end
+      end
+    end
+    if @user.helper.present?
+      if @user.helper.notifications == true  
+        mail(to: @user.email, subject: "You have received a new message.")
+      end
+    end
   end
 end

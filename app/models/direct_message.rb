d9ad0344d62788f59963b7785bddb6c6c71a64fa
jay johnson <jayj10080@gmail.com>
2018-03-05 00:09:06 +0000

    firebase for direct messages

--- app/models/direct_message.rb
+++ app/models/direct_message.rb
@@ -3,6 +3,7 @@ class DirectMessage < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :body, :conversation_id, :user_id
   after_create :send_message_email
+  after_create :send_to_firebase2
  
   def message_time
     created_at.strftime("%m/%e/%y %l:%M %p ")
@@ -10,6 +11,10 @@ class DirectMessage < ActiveRecord::Base
 
   def send_message_email
     NotificationMailer.new_message(self).deliver_now
+  end
+
+  def send_to_firebase2
+    FIREBASE.push("directmessage", { :id => self.id, :body => self.body, :'.priority' => 1, :timestamp => self.created_at })
   end
 
 end
